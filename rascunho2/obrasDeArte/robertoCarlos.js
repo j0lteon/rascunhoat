35da994f75b16befeb6ccf454513891d346b3f51
Jolteon KitKat <135230220+j0lteon@users.noreply.github.com>
2023-07-10 12:06:52 -0300

    Add files via upload

---
+++ rascunho2/obrasDeArte/robertoCarlos.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', ()=>{
+    document.querySelectorAll('.abas nav a').forEach((a)=>{
+        a.addEventListener('click',()=>{
+            let f=a.parentNode.querySelector('a.focus');
+            if(f)f.classList.remove('focus');
+            a.classList.add('focus');
+        });
+    });
+    if(location.hash){
+        document.querySelector('a[href="'+location.hash+'"]').classList.add('focus');
+    }
+});
+
+function playAudio(){
+    let x = document.getElementById("yuuriYOASOBI");
+    x.play();
+}
+
+function pauseAudio(){
+    let x = document.getElementById("yuuriYOASOBI");
+    x.pause();
+}
